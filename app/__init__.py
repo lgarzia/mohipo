73f771958fb7b7cf09011f8f6b69c0a8e192e8fb
Luke Garzia <lgarzia@yahoo.com>
2018-12-27 06:52:29 -0600

    Integrated Bootstrap4

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from ..config import config #top level file
+
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    Bootstrap(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
