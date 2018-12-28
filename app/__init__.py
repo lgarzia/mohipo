9548bc699cc65e56aea4e7309c95175f96d800ac
Luke Garzia <lgarzia@yahoo.com>
2018-12-28 06:26:58 -0600

    basics of first form

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from ..config import config #top level file
 from flask_moment import Moment
+
 
 bootstrap = Bootstrap()
 moment = Moment()
@@ -9,6 +11,7 @@ moment = Moment()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'A really long and hard to guess string' #TODO
     bootstrap.init_app(app)
     moment.init_app(app)
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,14 @@
 from datetime import datetime
 from flask import render_template, url_for
-
+from .forms import NameForm
 from . import main
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow()) #references "templates folder in app"
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name) #references "templates folder in app"
