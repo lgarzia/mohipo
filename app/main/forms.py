16c35ad613de76e992c915e2abedf74f0c17de7d
Luke Garzia <lgarzia@yahoo.com>
2018-12-29 11:41:32 -0600

    wiring for basic form/db completed

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import Required
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
+    role = StringField('What is your role?', validators=[Required()])
+    language = SelectField(u'Programming Language', choices=[('cpp', 'C++'), ('py', 'Python'), ('text', 'Plain Text')])
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -2,20 +2,30 @@ from datetime import datetime
 from flask import render_template, url_for, redirect, session, flash, current_app
 from .forms import NameForm
 from . import main
-
+from ..models import User, Role
+from .. import db
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    print(dir(current_app))
     name = None
-    form = NameForm()
-    print(f"before form validate {form.data}")
+    form = NameForm() #name/role
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.data['name']:
             flash('Looks like you changed your name')
         session['name'] = form.data['name']
-        print(f"after form validate {form.data} {session}")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            role = Role(name=form.role.data)
+            db.session.add(role)
+            db.session.commit()
+            role_id = role.id
+            print(f"role_id is {role_id}")
+            user = User(username=form.name.data, role_id=role_id)
+            db.session.add(user)
+            db.session.commit()
+            print(f"user_id is {user.id}")
+        print(f"user is {user}")
         form.name.data = ''
         return redirect(url_for('main.index'))
     return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name')) #references "templates folder in app"
